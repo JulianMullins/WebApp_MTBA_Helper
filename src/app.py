af5148904fbc1a562b0b253c32c99edc46fcb6ce
Julian Mullins <mullinsjulian@gmail.com>
2017-11-28 22:59:36 -0500

    initialize webapp setup & json functions

--- src/app.py
+++ src/app.py
@@ -1,16 +1,16 @@
 """
-Simple "Hello, World" application using Flask
+A Web App for locating the nearest MBTA station to a given location
 """
 
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
--- src/mbta_helper.py
+++ src/mbta_helper.py
@@ -5,7 +5,8 @@ import json
 # Useful URLs (you need to add the appropriate parameters for your requests)
 GMAPS_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 MBTA_BASE_URL = "http://realtime.mbta.com/developer/api/v2/stopsbylocation"
-MBTA_DEMO_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
+MBTA_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
+GMAPS_API_KEY = "AIzaSyBTI7VclW4BKfBrq9xyQIichYqFh5UWtis"
 
 
 # A little bit of scaffolding if you want to use it
@@ -15,7 +16,10 @@ def get_json(url):
     Given a properly formatted URL for a JSON web API request, return
     a Python JSON object containing the response to that request.
     """
-    pass
+    f = urllib.request.urlopen(url)
+    response_text = f.read().decode('utf-8')
+    response_data = json.loads(response_text)
+    return response_data
 
 
 def get_lat_long(place_name):
@@ -25,22 +29,19 @@ def get_lat_long(place_name):
     See https://developers.google.com/maps/documentation/geocoding/
     for Google Maps Geocode API URL formatting requirements.
     """
-    pass
+    
+    #create url
+    place_arr = place_name.split()
+    place_string = "+".join(place_arr)
+    params = "?address="+place_string+"&key="+GMAPS_API_KEY
+    url = GMAPS_BASE_URL + params
 
+    json_res = get_json(url)
+    
+    location = json_res['results'][0]['geometry']['location']
+    latitude = location['lat']
+    longitude = location['lng']
 
-def get_nearest_station(latitude, longitude):
-    """
-    Given latitude and longitude strings, return a (station_name, distance)
-    tuple for the nearest MBTA station to the given coordinates.
-    See http://realtime.mbta.com/Portal/Home/Documents for URL
-    formatting requirements for the 'stopsbylocation' API in 'MBTA-realtime API v2 Documentation'.
-    """
-    pass
+    return (latitude, longitude)
 
-
-def find_stop_near(place_name):
-    """
-    Given a place name or address, return the nearest MBTA stop and the 
-    distance from the given place to that stop.
-    """
-    pass
+# print(get_lat_long("Babson Park, Wellesley"))
