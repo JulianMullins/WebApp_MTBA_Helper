fb7c35242b8e1549f4a12b4cdc1998802769587c
Julian Mullins <mullinsjulian@gmail.com>
2017-11-29 15:12:38 -0500

    update web app and mbta api functions

--- src/app.py
+++ src/app.py
@@ -2,15 +2,30 @@
 A Web App for locating the nearest MBTA station to a given location
 """
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+from mbta_helper import get_lat_long, get_address, find_stop_near
+
 app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    if request.method == "POST":
+        address = request.form['address']
+
+        nearest_stop = find_stop_near(address)
+
+        if nearest_stop == "No stops nearby":
+            return render_template('results.html')
+        else:
+            return render_template('results.html', start=address, stop_name=nearest_stop[0], stop_address=nearest_stop[1], dist=round(nearest_stop[2], 2))
     return render_template('index.html')
+
+@app.route('/results')
+def results():
+    return render_template('results.html')
 
 if __name__ == '__main__':
     app.run()
--- src/mbta_helper.py
+++ src/mbta_helper.py
@@ -5,7 +5,7 @@ import json
 # Useful URLs (you need to add the appropriate parameters for your requests)
 GMAPS_BASE_URL = "https://maps.googleapis.com/maps/api/geocode/json"
 MBTA_BASE_URL = "http://realtime.mbta.com/developer/api/v2/stopsbylocation"
-MBTA_API_KEY = "wX9NwuHnZU2ToO7GmGR9uw"
+MBTA_API_KEY = "VRDnOahZm0eZUIltYACIGQ"
 GMAPS_API_KEY = "AIzaSyBTI7VclW4BKfBrq9xyQIichYqFh5UWtis"
 
 
@@ -29,6 +29,9 @@ def get_lat_long(place_name):
     See https://developers.google.com/maps/documentation/geocoding/
     for Google Maps Geocode API URL formatting requirements.
     """
+
+    if place_name == "":
+        return "No Address Given"
     
     #create url
     place_arr = place_name.split()
@@ -37,6 +40,9 @@ def get_lat_long(place_name):
     url = GMAPS_BASE_URL + params
 
     json_res = get_json(url)
+    
+    if len(json_res['results']) == 0:
+        return "No Results"
     
     location = json_res['results'][0]['geometry']['location']
     latitude = location['lat']
@@ -52,7 +58,7 @@ def get_address(lat, lng):
     """
 
     #create url
-    params = "?latlng="+lat+","+lng+"&key="+GMAPS_API_KEY
+    params = "?latlng=" + str(lat) + "," + str(lng) + "&key="+GMAPS_API_KEY
     url = GMAPS_BASE_URL + params
 
     json_res = get_json(url)
@@ -64,27 +70,46 @@ def get_address(lat, lng):
 
 def get_nearest_station(latitude, longitude):
     """
-    Given latitude and longitude strings, return a (station_name, distance)
+    Given latitude and longitude strings, return a (station_name, address, distance)
     tuple for the nearest MBTA station to the given coordinates.
     See http://realtime.mbta.com/Portal/Home/Documents for URL
     formatting requirements for the 'stopsbylocation' API in 'MBTA-realtime API v2 Documentation'.
     """
 
     #create url
-    params = "?api_key="+MBTA_API_KEY+"&format=json"
+    params = "?api_key="+MBTA_API_KEY+"&format=json&lat=" + str(latitude) + "&lon=" + str(longitude)
     url = MBTA_BASE_URL + params
 
     json_res = get_json(url)
+    print(json_res)
 
-    return json_res
+    if len(json_res['stop']) == 0:
+        return "No stops nearby"
+
+    station_name = json_res['stop'][0]['stop_name']
+    station_lat_lon = (json_res['stop'][0]['stop_lat'], json_res['stop'][0]['stop_lon'])
+    station_address = get_address(station_lat_lon[0], station_lat_lon[1])
+    distance = json_res['stop'][0]['distance']
+
+    return (station_name, station_address, float(distance))
 
 
 def find_stop_near(place_name):
     """
-    Given a place name or address, return the nearest MBTA stop and the 
+    Given a place name or address, return the nearest MBTA stop name, the stop address and the 
     distance from the given place to that stop.
     """
-    pass
+    lat_lon = get_lat_long(place_name)
+
+    if lat_lon == "No Address Given":
+        return "No stops nearby"
+
+    nearest_stop = get_nearest_station(lat_lon[0], lat_lon[1])
+    return nearest_stop
 
 
-# print(get_lat_long("Babson Park, Wellesley"))
+# print(find_stop_near("677 Centre Street, Jamaica Plain, MA 02130"))
+
+# print(find_stop_near("231 Forest St."))
+
+print(find_stop_near(""))
